947de0c6baa4232476c34029fd040326a96d93b9
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-10-03 09:59:25 +0300

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get ':page', to: 'welcome#show', constraints: { page: /contact/ }, as: :page
   get 'callback', to: 'sessions#create'
   resources :projects
   post 'webhook', to: 'rebases#create', as: :webhook
