6e1a42ad0f22dddf8fb0f99ac83e9c7dc6bcb23c
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-10-06 17:11:20 +0300

    Add redirect to projects path if user registered

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,16 @@
 class WelcomeController < ApplicationController
   skip_before_action :require_user
+  before_action :require_guest, only: :index
+
   layout 'landing'
+
   def show
     render params[:page]
+  end
+
+  private
+
+  def require_guest
+    redirect_to projects_path if current_user
   end
 end
