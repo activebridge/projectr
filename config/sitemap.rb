772bc605425bb558d69862d28a0a442596a1ac78
daryakuritsyna <darya@acive-bridge.com>
2016-10-20 15:03:58 +0300

    fix sitemap file

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,10 +2,6 @@ require 'rubygems'
 require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'https://projectr.io'
-SitemapGenerator::Sitemap.sitemaps_path = 'shared/'
 SitemapGenerator::Sitemap.create do
-  PAGES.each do |p|
-    add p, changefreq: :never
-  end
+  add '/projects', changefreq: 'daily', priority: 0.9
 end
-SitemapGenerator::Sitemap.ping_search_engines
