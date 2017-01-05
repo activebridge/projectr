25210651ab6779fc49b3e4f9c8890e20a4ccf25a
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2017-01-05 14:37:50 +0200

    Fix auto rebase

--- app/jobs/refresher_job.rb
+++ app/jobs/refresher_job.rb
@@ -1,9 +1,9 @@
 class RefresherJob < ApplicationJob
   queue_as :default
 
-  def perform(repo, base)
+  def perform(repo, base = '')
     user = Repo.find_by(name: repo).user
-    pulls = user.github.pulls(repo).select { |a| a['base']['ref'] == base }
+    pulls = base.present? ? user.github.pulls(repo).select { |a| a['base']['ref'] == base } : user.github.pulls(repo)
     pulls.each do |pull|
       pr = user.github.pull(repo, pull['number'])
       RebaserJob.perform_now('repository' => { 'full_name' => repo }, 'pull_request' => pr)
--- lib/github.rb
+++ lib/github.rb
@@ -4,8 +4,8 @@ class Github
   end
 
   def rebase
-    `rm -rf #{@rebase.repo}`
-    `git clone git@#{@rebase.repository_name.parameterize}:#{@rebase.repository_name}.git ./#{@rebase.repo}`
+    `git clone git@#{@rebase.repo.parameterize}:#{@rebase.repo}.git ./#{@rebase.repo}` unless File.exist?("./#{@rebase.repo}")
+    `cd ./#{@rebase.repo} && git pull origin`
     `cd ./#{@rebase.repo} && git checkout #{@rebase.head}`
     output = `cd ./#{@rebase.repo} && git rebase origin/#{@rebase.base}`
     `cd ./#{@rebase.repo} && git rebase --abort`
@@ -15,6 +15,7 @@ class Github
 
   def push
     `cd ./#{@rebase.repo} && git checkout #{@rebase.head}`
+    `cd ./#{@rebase.repo} && git fetch`
     `cd ./#{@rebase.repo} && git rebase origin/#{@rebase.base}`
     output = `cd ./#{@rebase.repo} && git push origin #{@rebase.head} --force-with-lease --porcelain`
     sha = `cd ./#{@rebase.repo} && git rev-parse HEAD`
